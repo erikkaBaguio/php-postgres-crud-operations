2ef50b879743ffa7003630333fad218d48e39df5
erikkaBaguio <erikka.baguio@gmail.com>
2018-07-20 14:19:10 +0800

    Added html table  for search or get or read a patient record

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 	
 	$dbConn = dbConnect("host=localhost dbname=hms user=postgres password=password");
 	$add_result = "";
+	$fname = "";
+	$lname = "";
+
 
 	# Add patient
 	if(isset($_POST["btn-add"]))
@@ -16,6 +19,20 @@
 			$add_result = '<div class="alert alert-success" role="alert">Added patient successfully!</div>';
 		}
 	}
+
+
+	# Search patient
+    if(isset($_GET['fname'])){
+    	$fname = trim($_GET['fname']);
+    }
+    if(isset($_GET['lname'])){
+    	$lname = trim($_GET['lname']);
+    }
+
+    # Get all patient
+	$patients = search_patients($_GET);
+	$display_patients = 0;
+
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +51,46 @@
 	<div class="container">
 		<br>
 		<br>
+		<div class="page-header">
+            <h1>Patients</h1>
+        </div>
+        <div class="row">
+        	<div class="col-sm-12">
+        		<form class="form-inline" action="" method="GET">
+				    <label class="sr-only" for="fname">First Name</label>
+				    <input type="text" class="form-control mb-2 mr-sm-2" id="fname" name="fname" value="<?php echo $fname; ?>" placeholder="Firstname...">
+				    <label class="sr-only" for="lname">Last Name</label>
+				    <input type="text" class="form-control mb-2 mr-sm-2" id="lname" name="lname" value="<?php echo $lname; ?>" placeholder="Latname..">
+				 
+				    <button type="submit" class="btn btn-primary mb-2" name="btn-search">Search</button>
+				</form>
+				<p>Records found: <?php echo count($patient); ?></p>
+        		<table class="table table-hover">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Name</th>
+							<th scope="col">Age</th>
+							<th scope="col">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+					        foreach ($patients as $key => $value) {
+					    ?>
+						<tr>
+							<th><?php echo $value['id'] ?></th>
+				            <td><?php echo $value['fname'] .' '. $value['mname'] .' '. $value['lname']; ?></td>
+				            <td><?php echo $value['age']; ?></td>
+				            <td><a href="includes/html/patient.update.inc.php?id=<?php echo $value['id']; ?>" class="btn btn-info m-r-1em">Update</a></td>
+						</tr>
+						<?php
+							}
+						?>
+					</tbody>
+				</table>	
+        	</div>
+        </div>
 		<?php include 'includes/html/patient.add.inc.php';?>
 	</div>
 	     
