02d6a55351a1ba19073aec49bdf6d1819d777811
erikkaBaguio <erikka.baguio@gmail.com>
2018-07-20 12:20:00 +0800

    Added function for input sanitation

--- includes/functions/patient.func.php
+++ includes/functions/patient.func.php
@@ -7,7 +7,7 @@
 	{
 		global $dbConn;
 		$result = [];
-		$debug = 1;
+		$debug = 0;
 
 
 		$sql = "CREATE TABLE IF NOT EXISTS Patient(
@@ -27,6 +27,17 @@
 		} else {
 			return pg_last_error($dbConn);
 		}		
+	}
+
+
+	function sanitize_str($str)
+	# Author: Ma. Erikka M. Baguio
+	# Date created: July 20, 2018
+	# Date modified: July 20, 2018
+	# Description: Sanitize inputs to avoid SQL Injection 
+	{
+		$str = str_replace("'", "''", trim($str));
+		return $str;
 	}
 
 ?>
