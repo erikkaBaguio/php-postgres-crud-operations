a83b78acdbdb984281f9ebfea8ae806e35378554
erikkaBaguio <erikka.baguio@gmail.com>
2018-07-26 18:14:49 +0800

    Tried namespaces

---
+++ file1.php
@@ -0,0 +1,12 @@
+<?php
+namespace File1;
+
+const FOO = 1;
+function foo() {
+	echo "foo file1";
+}
+class foo
+{
+    static function staticmethod() {}
+}
+?>
---
+++ file2.php
@@ -0,0 +1,13 @@
+<?php
+namespace File2;
+
+const FOO = 2;
+function foo() {
+	echo "foo file2";
+}
+class foo
+{
+    static function staticmethod() {}
+}
+
+?>
